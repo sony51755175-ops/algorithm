25122038337db3f1c11a714b8525e5041a78e2dd
Beforejamn1 <beforejamn1@gmail.com>
2026-02-10 22:12:12 +0900

    FEAT: BOJ_2630_색종이 만들기-PASS

---
+++ 0212/basic_B2630_색종이_만들기/JaeMin.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JaeMin {
+    static int N, total, paper[][];
+    static int blues, whites;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        paper = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solve(0, 0, N);
+
+        System.out.println(sb.append(whites).append("\n").append(blues));
+
+    }
+
+    public static void solve(int r, int c, int size){
+        if(!isDivide(r, c, size)){
+            if(paper[r][c] == 0) whites++;
+            else blues++;
+            return;
+        }
+            
+        int nSize = size>>1;
+        solve(r, c, nSize);
+        solve(r + nSize, c, nSize);
+        solve(r, c+ nSize, nSize);
+        solve(r + nSize, c + nSize, nSize);
+    }
+    
+    private static boolean isDivide(int r, int c, int size){
+        int before = paper[r][c];
+
+        for (int i = r; i < r+ size ; i++) {
+            for (int j = c; j < c + size; j++) {
+                if(before != paper[i][j]) return true;
+            }
+        }
+        return false;
+    }
+}
